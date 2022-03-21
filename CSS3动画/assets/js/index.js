69430f9ec8634684aeb279c9d0552eb20ab80ea4
beitaandbaozi <2280496040@qq.com>
2022-03-21 23:36:05 +0800

    使用原生事件，增强代码可读性，也方便后期返工查找

--- CSS3动画/assets/js/index.js
+++ CSS3动画/assets/js/index.js
@@ -1,6 +1,29 @@
 var showFlag = false;
-$('.open').click(() => {
-  console.log('ok')
+// $('.open').click(() => {
+//   console.log('ok')
+//   if (!showFlag) {
+//     $('.nav').css({
+//       transform: 'translateX(-200px)',
+//       transition: 'all 1s'
+//     })
+//     $('.open').css({
+//       transform: 'translateX(-200px)',
+//       transition: 'all 1s'
+//     })
+//     showFlag = true
+//   } else {
+//     $('.nav').css({
+//       transform: 'translateX(0px)',
+//       transition: 'all 1s'
+//     })
+//     $('.open').css({
+//       transform: 'translateX(0px)',
+//       transition: 'all 1s'
+//     })
+//     showFlag = false
+//   }
+// })
+function openNav() {
   if (!showFlag) {
     $('.nav').css({
       transform: 'translateX(-200px)',
@@ -22,4 +45,4 @@ $('.open').click(() => {
     })
     showFlag = false
   }
-})
+}
