c072c0a1fe3df43fcef9496c739732c7059ef3fc
beitaandbaozi <2280496040@qq.com>
2022-05-17 21:56:38 +0800

    浅拷贝

---
+++ 拷贝/浅拷贝/index.js
@@ -0,0 +1,20 @@
+// 浅拷贝的思路就是
+// TODO 判断区分是引用类型或者不是引用类型，然后直接赋值就行了。是引用类型的话需要新创建一个
+let obj = {
+  name: "wupeixin",
+};
+
+function clone(obj) {
+  if (obj == null) return;
+  //TODO 判断是否是引用类型
+  // 不是引用类型
+  if (typeof obj != "object") return obj;
+  // 引用类型
+  let newObj = Array.isArray(obj) ? [] : {};
+  for (let key in obj) {
+    newObj[key] = obj[key];
+  }
+  return newObj;
+}
+
+const newObj = clone(obj);
